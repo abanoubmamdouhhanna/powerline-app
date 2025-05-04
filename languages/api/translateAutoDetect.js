39563b9ca5015c7e3ac1838c0f692de2163c2092
abanoubmamdouhhanna <beboabanoub332001@gmail.com>
2025-05-04 22:50:08 +0300

    googleCredentials

--- languages/api/translateAutoDetect.js
+++ languages/api/translateAutoDetect.js
@@ -2,9 +2,6 @@
 import { TranslationServiceClient } from '@google-cloud/translate';
 import { googleCredentials } from './powerline.js';
 
-// const client = new TranslationServiceClient({
-//   keyFilename: "../../../../projects/powerlineapp/languages/api/powerline.json",
-// });
 const client = new TranslationServiceClient({
   credentials: googleCredentials,
 });
@@ -27,38 +24,3 @@ const translateAutoDetect = async (text, targetLang) => {
 };
 
 export default translateAutoDetect;
-
-// import { TranslationServiceClient } from '@google-cloud/translate';
-
-// let client;
-
-// if (process.env.GOOGLE_TRANSLATE_CREDENTIALS) {
-//   const keyJson = Buffer.from(process.env.GOOGLE_TRANSLATE_CREDENTIALS, 'base64').toString('utf8');
-//   client = new TranslationServiceClient({
-//     credentials: JSON.parse(keyJson),
-//   });
-// } else {
-//   // Fallback for local development
-//   client = new TranslationServiceClient({
-//     keyFilename: './languages/api/powerline.json', // Adjust path if needed
-//   });
-// }
-
-// const location = 'global';
-
-// const translateAutoDetect = async (text, targetLang) => {
-//   const request = {
-//     parent: `projects/${process.env.GOOGLE_PROJECT_ID}/locations/${location}`,
-//     contents: [text],
-//     mimeType: 'text/plain',
-//     targetLanguageCode: targetLang,
-//   };
-
-//   const [response] = await client.translateText(request);
-//   const translatedText = response.translations[0].translatedText;
-//   const detectedLang = response.translations[0].detectedLanguageCode;
-
-//   return { translatedText, detectedLang };
-// };
-
-// export default translateAutoDetect;
